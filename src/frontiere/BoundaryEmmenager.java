243d4a8aeb7faf64f7304bbc5915ce881c7d072c
Alexandre Fort <alexandre.fort31@gmail.com>
2023-10-12 09:43:45 +0200

    until PrendreEtal

--- src/frontiere/BoundaryEmmenager.java
+++ src/frontiere/BoundaryEmmenager.java
@@ -27,12 +27,11 @@ public class BoundaryEmmenager {
 					break;
 
 				case 2:
-					//TODO a completer
+					emmenagerVillageois(nomVisiteur);
 					break;
 
 				default:
-					System.out
-							.println("Vous devez choisir le chiffre 1 ou 2 !");
+					System.out.println("Vous devez choisir le chiffre 1 ou 2 !");
 					break;
 				}
 			} while (choixUtilisateur != 1 && choixUtilisateur != 2);
@@ -40,6 +39,56 @@ public class BoundaryEmmenager {
 	}
 
 	private void emmenagerDruide(String nomVisiteur) {
-		//TODO a completer
+		StringBuilder creationForce = new StringBuilder();
+		StringBuilder creationPotionMin = new StringBuilder();
+		StringBuilder creationPotionMax = new StringBuilder();
+		
+		creationForce.append("Bienvenue druide " + nomVisiteur + "\n");
+		creationForce.append("Quelle est votre force ?");
+		int force = Clavier.entrerEntier(creationForce.toString());
+		
+		creationPotionMin.append("Quelle est la force de potion la plus faible que vous produisez ?");
+		creationPotionMax.append("Quelle est la force de potion la plus forte que vous produisez ?");
+		int effetPotionMin = -1;
+		int effetPotionMax = -1;
+		do {
+			effetPotionMin = Clavier.entrerEntier(creationPotionMin.toString());
+			effetPotionMax = Clavier.entrerEntier(creationPotionMax.toString());
+			if (effetPotionMax < effetPotionMin) {
+				System.out.println("Attention Druide, vous vous êtes trompé entre le minimum et le maximum !");
+			}
+			
+		} while (effetPotionMax < effetPotionMin);
+		
+		controlEmmenager.ajouterDruide(nomVisiteur, force, effetPotionMin, effetPotionMax);
+	}
+	
+	private void emmenagerVillageois(String nomVisteur) {
+		StringBuilder creationForce = new StringBuilder();
+		
+		creationForce.append("Bienvenue villageois " + nomVisteur + "\n");
+		creationForce.append("Quelle est votre force ?");
+		int force = Clavier.entrerEntier(creationForce.toString());
+		
+		controlEmmenager.ajouterGaulois(nomVisteur, force);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/frontiere/BoundaryPrendreEtal.java
+++ src/frontiere/BoundaryPrendreEtal.java
@@ -3,9 +3,11 @@ package frontiere;
 import java.util.Scanner;
 
 import controleur.ControlPrendreEtal;
+import controleur.ControlVerifierIdentite;
 
 public class BoundaryPrendreEtal {
 	private ControlPrendreEtal controlPrendreEtal;
+	private ControlVerifierIdentite controlVerifierIdentite;
 	private Scanner scan = new Scanner(System.in);
 
 	public BoundaryPrendreEtal(ControlPrendreEtal controlChercherEtal) {
@@ -13,7 +15,14 @@ public class BoundaryPrendreEtal {
 	}
 
 	public void prendreEtal(String nomVendeur) {
-		//TODO a completer
+		if (!controlVerifierIdentite.verifierIdentite(nomVendeur)) {
+			System.out.println("Je suis désolé " + nomVendeur
+					+ " mais il faut être une habitant de notre village pour commercer ici");
+		}
+		else {
+			System.out.println("Bonjour " + nomVendeur
+					+ " je vais regarder si je peux vous trouver un étal");
+		}
 	}
 
 	private void installerVendeur(String nomVendeur) {
